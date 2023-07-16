ae78d741bbc9967e0c7dd00e26d50ff1eb78456f
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-16 18:22:53 -0400

    filled out shapes.js file

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Triangle, Square} = require("./lib/shapes");
+const {Circle, Triangle, Square} = require("./lib/shapes.js");
 
 
-const questtions = [
-{
-    type: "input",
-    message: "Please enter up to 3 characters for your logo",
-    name: "text",
-},
-{
-    type: "input",
-    message: "Please enter a color keyword OR a hexadecimal number for the text",
-    name: "textColor",
-},
-{
-   type: "list",
-   message: "Please select a shape for your logo:",
-   choices: ['circle', 'triangle', 'square'],
-   name: "shape"
-},
-{
-    type: "input",
-    message: "Please enter a color keyword OR hexadecimal number for the shape",
-    name: "shapeColor"
-},
-
+const questions = [
+    {
+        type: "input",
+        message: "Please enter up to 3 characters for your logo",
+        name: "text",
+    },
+    {
+        type: "input",
+        essage: "Please enter a color keyword OR a hexadecimal number for the text",
+        name: "textColor",
+    },
+    {
+        type: "list",
+        message: "Please select a shape for your logo:",
+        choices: ['circle', 'triangle', 'square'],
+        name: "shape",
+    },
+    {
+        type: "input",
+        message: "Please enter a color keyword OR hexadecimal number for the shape",
+        name: "shapeColor",
+    },
 
 ];
 
