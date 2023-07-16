5894d7565272469639455a0f3aeb26066388aa88
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-16 16:11:40 -0400

    added more files and added questions array to index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {Circle, Triangle, Square} = require("./lib/shapes");
+
+
+const questtions = [
+{
+    type: "input",
+    message: "Please enter up to 3 characters for your logo",
+    name: "text",
+},
+{
+    type: "input",
+    message: "Please enter a color keyword OR a hexadecimal number for the text",
+    name: "textColor",
+},
+{
+   type: "list",
+   message: "Please select a shape for your logo:",
+   choices: ['circle', 'triangle', 'square'],
+   name: "shape"
+},
+{
+    type: "input",
+    message: "Please enter a color keyword OR hexadecimal number for the shape",
+    name: "shapeColor"
+},
+
+
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log("created file")
+    })
+};
+
